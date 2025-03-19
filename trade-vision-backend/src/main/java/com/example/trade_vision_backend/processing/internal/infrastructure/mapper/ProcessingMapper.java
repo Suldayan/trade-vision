6c38300bef02c3884a40c6b995f47dcf0d0d9801
Timothy <timothysuldayan1@gmail.com>
2025-03-18 22:06:16 -0700

    configured mapper

---
+++ trade-vision-backend/src/main/java/com/example/trade_vision_backend/processing/internal/infrastructure/mapper/ProcessingMapper.java
@@ -0,0 +1,16 @@
+package com.example.trade_vision_backend.processing.internal.infrastructure.mapper;
+
+import com.example.trade_vision_backend.processing.ProcessedMarketDTO;
+import com.example.trade_vision_backend.processing.ProcessedMarketModel;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper
+public interface ProcessingMapper {
+    ProcessingMapper INSTANCE = Mappers.getMapper(ProcessingMapper.class);
+
+    ProcessedMarketDTO entityToDto(ProcessedMarketModel processedMarketModel);
+    List<ProcessedMarketDTO> entityListToDtoList(List<ProcessedMarketModel> processedMarketModels);
+}
