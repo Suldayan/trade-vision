641fbea8073172fb002a814803b552ba3c8b8c2e
Timothy <timothysuldayan1@gmail.com>
2025-03-25 21:39:13 -0700

    created a candle model to keep track of closing prices

---
+++ trade-vision-backend/src/main/java/com/example/trade_vision_backend/processing/ProcessedCandleModel.java
@@ -0,0 +1,51 @@
+package com.example.trade_vision_backend.processing;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "candle_markets")
+public class ProcessedCandleModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "base_id", nullable = false)
+    private String baseId;
+
+    @Column(name = "quote_id", nullable = false)
+    private String quoteId;
+
+    @Column(name = "exchange_id", nullable = false)
+    private String exchangeId;
+
+    @Column(name = "price_usd", nullable = false)
+    private BigDecimal priceUsd;
+
+    @Column(name = "closing_price_usd")
+    private BigDecimal closingPriceUsd;
+
+    @Column(name = "updated", nullable = false)
+    private Long updated;
+
+    @Version
+    @Column(name = "version")
+    private Integer version;
+
+    @Column(name = "timestamp", nullable = false)
+    private ZonedDateTime timestamp;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+}
