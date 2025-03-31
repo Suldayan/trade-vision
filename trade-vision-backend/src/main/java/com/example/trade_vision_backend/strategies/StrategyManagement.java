55dc6e86a337f2f2d0bec4ff0d861c35f9e6ee53
Timothy <timothysuldayan1@gmail.com>
2025-03-30 22:36:06 -0700

    configured module listner for strategy events

--- trade-vision-backend/src/main/java/com/example/trade_vision_backend/strategies/StrategyManagement.java
+++ trade-vision-backend/src/main/java/com/example/trade_vision_backend/strategies/StrategyManagement.java
@@ -1,15 +1,19 @@
 package com.example.trade_vision_backend.strategies;
 
+import com.example.trade_vision_backend.backtester.StrategyEvent;
+import jakarta.annotation.Nonnull;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.modulith.events.ApplicationModuleListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class StrategyManagement {
 
     @ApplicationModuleListener
-    public void executeStrategy() {
+    public void executeStrategy(@Nonnull StrategyEvent event) {
 
     }
 }
