5e82c39a0ee3efc6ed19306d1cd49ba5157ad807
Timothy <timothysuldayan1@gmail.com>
2025-04-06 17:02:27 -0700

    created area for historical data to be saved

--- trade-vision-backtester/strategy_executor.py
+++
@@ -1,7 +0,0 @@
-from sklearn.ensemble import RandomForestClassifier
-
-class StrategyExecutor:
-    def __init__(self, id, buyCondition, sellCondition, riskPerTrade):
-        self.id = id
-    
-        pass
