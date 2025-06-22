0ceebc4dfddb5cb09dbaccbc7d851988a0988ae3
Timothy <timothysuldayan1@gmail.com>
2025-06-21 18:30:59 -0700

    added spring batch

---
+++ trade-vision-backend/src/main/java/com/example/trade_vision_backend/market/internal/BatchConfig.java
@@ -0,0 +1,9 @@
+package com.example.trade_vision_backend.market.internal;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+}
